b02732b86e72269975d60d0cb3d7fc05cb7eb094
sumitshinde2608 <shindesumit268@gmail.com>
2022-01-16 03:30:51 +0530

    Basic Editor Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Editor from "./components/Editor/Editor";
+import Header from "./components/Header/Header";
+import "./App.css";
 
 const App = () => {
 	return (
 		<div>
-			<Editor />
+			<div>
+				<Header />
+			</div>
+			<div className='codearea'>
+				<Editor />
+			</div>
 		</div>
 	);
 };
